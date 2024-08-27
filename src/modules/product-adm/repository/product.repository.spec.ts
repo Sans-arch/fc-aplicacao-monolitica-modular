bd91fe5244f42f0838582b92970c7b119ec0ba4c
Santiago Fleitas <sansfleitas@gmail.com>
2024-08-26 21:31:07 -0300

    Create ProductModel (infra), create ProductRepository

---
+++ src/modules/product-adm/repository/product.repository.spec.ts
@@ -0,0 +1,24 @@
+import { Sequelize } from "sequelize-typescript";
+import { ProductModel } from "./product.model";
+
+describe("ProductRepository test", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should create a product", async () => {});
+});
