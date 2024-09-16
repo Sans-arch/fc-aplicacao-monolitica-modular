d339a0ca1b98b185f1690c81154d93de0aee4884
Santiago Fleitas <sansfleitas@gmail.com>
2024-09-16 09:59:03 -0300

    Create Find All Products usecase

---
+++ src/modules/store-catalog/usecase/find-all-products/find-all-products.usecase.ts
@@ -0,0 +1,24 @@
+import UseCaseInterface from "../../../@shared/usecase/use-case.interface";
+import ProductGateway from "../../gateway/product.gateway";
+import { FindAllProductsDto } from "./find-all-products.dto";
+
+export default class FindAllProductsUsecase implements UseCaseInterface {
+  private _productRepository: ProductGateway;
+
+  constructor(productRepository: ProductGateway) {
+    this._productRepository = productRepository;
+  }
+
+  async execute(): Promise<FindAllProductsDto> {
+    const products = await this._productRepository.findAll();
+
+    return {
+      products: products.map((product) => ({
+        id: product.id.id,
+        name: product.name,
+        description: product.description,
+        salesPrice: product.salesPrice,
+      })),
+    };
+  }
+}
