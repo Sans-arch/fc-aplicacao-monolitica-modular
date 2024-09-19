fefbe3335ee324a3ee2c4dc3e5f43ed82a4eca83
Santiago Negreira <sansfleitas@gmail.com>
2024-09-19 14:59:16 -0300

    Create entities of invoice module

---
+++ src/modules/invoice/domain/invoice.ts
@@ -0,0 +1,28 @@
+import AggregateRoot from "../../@shared/domain/entity/aggregate-root.interface";
+import BaseEntity from "../../@shared/domain/entity/base.entity";
+import Id from "../../@shared/domain/value-object/id.value-object";
+import Address from "./address";
+import InvoiceItem from "./invoice_item";
+
+interface InvoiceProps {
+  id?: Id;
+  name: string;
+  document: string;
+  address: Address;
+  items: InvoiceItem[];
+}
+
+export default class Invoice extends BaseEntity implements AggregateRoot {
+  private _name: string;
+  private _document: string;
+  private _address: Address;
+  private _items: InvoiceItem[];
+
+  constructor(props: InvoiceProps) {
+    super(props.id);
+    this._name = props.name;
+    this._document = props.document;
+    this._address = props.address;
+    this._items = props.items;
+  }
+}
---
+++ src/modules/invoice/domain/invoice_item.ts
@@ -0,0 +1,19 @@
+import BaseEntity from "../../@shared/domain/entity/base.entity";
+import Id from "../../@shared/domain/value-object/id.value-object";
+
+interface InvoiceItemProps {
+  id?: Id;
+  name: string;
+  price: number;
+}
+
+export default class InvoiceItem extends BaseEntity {
+  private _name: string;
+  private _price: number;
+
+  constructor(props: InvoiceItemProps) {
+    super(props.id);
+    this._name = props.name;
+    this._price = props.price;
+  }
+}
