9f0ee0315ace30890d2c9c0036fcca7eea705473
Santiago Negreira <sansfleitas@gmail.com>
2024-09-23 19:14:40 -0300

    Update place order

--- src/modules/checkout/usecase/place-order/place-order.usecase.spec.ts
+++ src/modules/checkout/usecase/place-order/place-order.usecase.spec.ts
@@ -1,7 +1,87 @@
 import { PlaceOrderInputDto } from "./place-order.dto";
 import PlaceOrderUsecase from "./place-order.usecase";
 
+const mockDate = new Date(2000, 1, 1);
+
 describe("PlaceOrderUsecase unit test", () => {
+  describe("validateProducts method", () => {
+    // @ts-expect-error - no params in constructor
+    const placeOrderUsecase = new PlaceOrderUsecase();
+
+    it("should throw error if no products are selected", async () => {
+      const input: PlaceOrderInputDto = {
+        clientId: "0",
+        products: [],
+      };
+
+      await expect(placeOrderUsecase["validateProducts"](input)).rejects.toThrow(new Error("No products selected"));
+    });
+
+    it("should throw an error when product is out of stock", async () => {
+      const mockProductFacade = {
+        checkStock: jest.fn(({ productId }: { productId: string }) => {
+          return Promise.resolve({
+            productId,
+            stock: productId === "1" ? 0 : 1,
+          });
+        }),
+      };
+
+      // @ts-expect-error - force set productFacade
+      placeOrderUsecase["_productFacade"] = mockProductFacade;
+
+      let input: PlaceOrderInputDto = {
+        clientId: "0",
+        products: [{ productId: "1" }],
+      };
+
+      await expect(placeOrderUsecase["validateProducts"](input)).rejects.toThrow(
+        new Error("Product 1 is not available in stock")
+      );
+
+      input = {
+        clientId: "0",
+        products: [{ productId: "0" }, { productId: "1" }],
+      };
+
+      await expect(placeOrderUsecase["validateProducts"](input)).rejects.toThrow(
+        new Error("Product 1 is not available in stock")
+      );
+
+      expect(mockProductFacade.checkStock).toHaveBeenCalledTimes(3);
+
+      input = {
+        clientId: "0",
+        products: [{ productId: "1" }, { productId: "2" }],
+      };
+      await expect(placeOrderUsecase["validateProducts"](input)).rejects.toThrow(
+        new Error("Product 1 is not available in stock")
+      );
+
+      expect(mockProductFacade.checkStock).toHaveBeenCalledTimes(4);
+    });
+  });
+
+  describe("getProducts method", () => {
+    beforeAll(() => {
+      jest.useFakeTimers({ advanceTimers: true });
+      jest.setSystemTime(mockDate);
+    });
+
+    afterAll(() => {
+      jest.useRealTimers();
+    });
+
+    // @ts-expect-error - no params in constructor
+    const placeOrderUsecase = new PlaceOrderUsecase();
+
+    it("should throw an error when product not found", async () => {
+      const mockCatalogFacade = {
+        find: jest.fn(),
+      };
+    });
+  });
+
   describe("execute method", () => {
     it("should throw an error when client not found", async () => {
       const mockClientFacade = {
@@ -22,7 +102,7 @@ describe("PlaceOrderUsecase unit test", () => {
 
     it("should throw an error when products are not valid", async () => {
       const mockClientFacade = {
-        find: jest.fn().mockResolvedValue(null),
+        find: jest.fn().mockResolvedValue(true),
       };
 
       // @ts-expect-error
@@ -40,7 +120,7 @@ describe("PlaceOrderUsecase unit test", () => {
         products: [],
       };
 
-      await expect(placeOrderUsecase).rejects.toThrow(new Error("No products selected"));
+      await expect(placeOrderUsecase.execute(input)).rejects.toThrow(new Error("No products selected"));
 
       expect(mockValidateProducts).toHaveBeenCalledTimes(1);
     });
--- src/modules/checkout/usecase/place-order/place-order.usecase.ts
+++ src/modules/checkout/usecase/place-order/place-order.usecase.ts
@@ -1,12 +1,15 @@
 import UseCaseInterface from "../../../@shared/usecase/use-case.interface";
 import ClientAdmFacadeInterface from "../../../client-adm/facade/client-adm.facade.interface";
+import { ProductAdmFacadeInterface } from "../../../product-adm/facade/product-adm.facade.interface";
 import { PlaceOrderInputDto, PlaceOrderOutputDto } from "./place-order.dto";
 
 export default class PlaceOrderUsecase implements UseCaseInterface {
   private _clientFacade: ClientAdmFacadeInterface;
+  private _productFacade: ProductAdmFacadeInterface;
 
-  constructor(clientFacade: ClientAdmFacadeInterface) {
+  constructor(clientFacade: ClientAdmFacadeInterface, productFacade: ProductAdmFacadeInterface) {
     this._clientFacade = clientFacade;
+    this._productFacade = productFacade;
   }
 
   async execute(input: PlaceOrderInputDto): Promise<PlaceOrderOutputDto> {
@@ -16,7 +19,7 @@ export default class PlaceOrderUsecase implements UseCaseInterface {
       throw new Error("Client not found");
     }
 
-    // validar produtos.
+    await this.validateProducts(input);
     // recuperar os produtos
 
     // criar objeto do client
@@ -35,5 +38,21 @@ export default class PlaceOrderUsecase implements UseCaseInterface {
       total: 100,
       products: input.products,
     };
+  }
+
+  private async validateProducts(input: PlaceOrderInputDto): Promise<void> {
+    if (input.products.length === 0) {
+      throw new Error("No products selected");
+    }
+
+    for (const p of input.products) {
+      const product = await this._productFacade.checkStock({
+        productId: p.productId,
+      });
+
+      if (product.stock <= 0) {
+        throw new Error(`Product ${product.productId} is not available in stock`);
+      }
+    }
   }
 }
